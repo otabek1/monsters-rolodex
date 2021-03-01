efb47c9b41ddfec755bb28129ab6148990dfd47e
Otabek <abduraufov_otabek@mail.ru>
2021-03-02 01:44:07 +0500

    finished

--- src/Components/Card/card.jsx
+++ src/Components/Card/card.jsx
@@ -3,7 +3,7 @@ import './card.css'
 export const Card = (props) =>  (
     <div className="card-container">
         <img alt="monster"
-        src={`https://robohash.org/${props.monster.id}?set=set2%size=180x180`} />
+        src={`https://robohash.org/${props.monster.id}?set=set4?size=100x100`} />
         <h2 >{props.monster.name}</h2>
         <h2>{props.monster.email}</h2>
 
